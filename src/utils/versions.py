dcd23b91bfee20a13ae4075174a4aa4baa8c5ded
Nikita Serba <nikitaserba@icloud.com>
2026-08-18 18:14:42 +0200

    Add basic code to work with versions

---
+++ src/utils/versions.py
@@ -0,0 +1,26 @@
+# PsySymTrack
+# Psychiatric symptom tracker with basic analysis
+# Copyright (C) 2026 Nikita Serba. All rights reserved
+# https://github.com/sandsbit/PsySymTrack
+#
+# PsySymTrack is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+#
+# PsySymTrack is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PsySymTrack. If not, see <https://www.gnu.org/licenses/>.
+
+def process_release_version(version: str) -> tuple[int, int, int]:
+    if "-" in version:
+        version = version[0:version.index("-")]
+    numbers = version.split(".")
+    if len(numbers) != 3:
+        raise ValueError("Invalid version given: " + version)
+
+    return tuple(map(int, numbers))
